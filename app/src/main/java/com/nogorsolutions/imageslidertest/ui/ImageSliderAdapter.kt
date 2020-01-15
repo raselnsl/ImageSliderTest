7cdac7d354fe03aa2ade2a25eef91d6734936d76
raselnsl <rasel@nogorsolutions.com>
2020-01-15 19:12:10 +0600

    Initial commit

---
+++ app/src/main/java/com/nogorsolutions/imageslidertest/ui/ImageSliderAdapter.kt
@@ -0,0 +1,46 @@
+package com.nogorsolutions.imageslidertest.ui
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.nogorsolutions.imageslidertest.R
+import kotlinx.android.synthetic.main.image_slider_item.view.*
+
+class ImageSliderAdapter(private val context: Context) : PagerAdapter() {
+
+
+    private var inflater: LayoutInflater? = null
+    private val images = arrayOf(R.drawable.cesc_1, R.drawable.cesc_2, R.drawable.cesc_3, R.drawable.cesc_4)
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+
+        return view === `object`
+    }
+
+    override fun getCount(): Int {
+
+        return images.size
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+
+        inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater!!.inflate(R.layout.image_slider_item, null)
+        view.imageView_slide.setImageResource(images[position])
+
+        val vp = container as ViewPager
+        vp.addView(view, 0)
+        return view
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+
+        val vp = container as ViewPager
+        val view = `object` as View
+        vp.removeView(view)
+    }
+
+}
